e8f33a8b10056164c1a51cb68d2466f63e2b4cf9
annaboyatyuk <annaboyatyuk@gmail.com>
2018-06-09 17:30:47 -0700

    i keep forgetting to switch branches, i will write myself a note

---
+++ src/lib/storage/data-store.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const memoryStorage = require('./memory.js');
+const fileStorage = require('./filesystem.js');
+
+let dataStorageModule = {};
+
+
+switch( process.env.STORAGE ) {
+case 'filesystem':
+  dataStorageModule = fileStorage;
+  break;
+default:
+  dataStorageModule = memoryStorage;
+  break;
+}
+
+module.exports = dataStorageModule;
---
+++ src/models/notes.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const storage = require('../lib/storage/data-store.js');
+const uuid = require('uuid/v1');
+
+class Note{
+
+  constructor(config) {
+    this.id = uuid();
+    this.createdOn = new Date();
+    this.title = config && config.title || '';
+    this.name = config && config.name || '';
+    this.content = config && config.content || '';
+  }
+
+
+  save() {
+    return storage.save(this);
+  }
+
+  static fetchAll() {
+    return storage.getAll();
+  }
+
+  static findOne(id) {
+    return storage.get(id);
+  }
+
+  static updateOne(criteria) {
+    return storage.update(this);
+  }
+
+  static deleteOne(id) {
+    return storage.delete(id);
+  }
+
+}
+
+module.exports = Note;
